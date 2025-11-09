22cde4e4dc074b10e45f0c243acf495a4881714f
_.yzero <116497775+FISTOFDARKNESS@users.noreply.github.com>
2025-11-09 09:35:09 +0100

    Update _db.js

--- netlify/functions/_db.js
+++ netlify/functions/_db.js
@@ -1,8 +1,10 @@
 const { Client } = require("pg");
+
 function getClient() {
   return new Client({
     connectionString: "postgresql://neondb_owner:npg_RVcjEu4DI3mJ@ep-dawn-tree-ad1airj2-pooler.c-2.us-east-1.aws.neon.tech/KeySytem?sslmode=require&channel_binding=require",
     ssl: { rejectUnauthorized: false }
   });
 }
+
 module.exports = { getClient };
