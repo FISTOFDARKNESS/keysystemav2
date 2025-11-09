511c03a482f241b54b64eec513dff709f0085c7e
_.yzero <116497775+FISTOFDARKNESS@users.noreply.github.com>
2025-11-09 02:01:01 +0100

    Update _db.js

--- netlify/functions/_db.js
+++ netlify/functions/_db.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg');
 
 function getClient() {
-  const connectionString = process.env.DATABASE_URL;
+  const connectionString = 'postgresql://neondb_owner:npg_RVcjEu4DI3mJ@ep-dawn-tree-ad1airj2-pooler.c-2.us-east-1.aws.neon.tech/KeySytem?sslmode=require&channel_binding=require';
   return new Client({
     connectionString,
     ssl: { rejectUnauthorized: false }
