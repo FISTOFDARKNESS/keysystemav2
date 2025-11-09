19a09d4429c7435060f91dfd6bc567e02576b590
_.yzero <116497775+FISTOFDARKNESS@users.noreply.github.com>
2025-11-09 02:15:03 +0100

    Update _db.js

--- netlify/functions/_db.js
+++ netlify/functions/_db.js
@@ -1,9 +1,10 @@
 const { Client } = require('pg');
 
+const CONNECTION_STRING = 'postgresql://neondb_owner:npg_RVcjEu4DI3mJ@ep-dawn-tree-ad1airj2-pooler.c-2.us-east-1.aws.neon.tech/KeySytem?sslmode=require&channel_binding=require';
+
 function getClient() {
-  const connectionString = 'postgresql://neondb_owner:npg_RVcjEu4DI3mJ@ep-dawn-tree-ad1airj2-pooler.c-2.us-east-1.aws.neon.tech/KeySytem?sslmode=require&channel_binding=require';
   return new Client({
-    connectionString,
+    connectionString: CONNECTION_STRING,
     ssl: { rejectUnauthorized: false }
   });
 }
